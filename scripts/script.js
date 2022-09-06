07dee0ca2f8911823105fb4c98acad4840c49f49
Justin Hairston <jhairst2@vols.utk.edu>
2022-09-05 20:31:28 -0400

    Add button functionality for the numbers

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,29 @@
 // Figure out a way to get buttons to display value on screen
+// Variables
+const screen = document.querySelector(".screen");
+const numButtons = document.querySelectorAll(".num-pad");
+
+// Event Listeners
+numButtons.forEach(button => {
+    button.addEventListener("click", appendNum) 
+})
+
+
+
 // save values for calculations
 // Do calculations with the values selected
 // get math operations to do perform correctly
 // Clear and delete buttons
 // display errors when appropriate.
+
+
+// Functions
+
+function appendNum() {
+    if (screen.textContent == 0) {
+        screen.textContent = this.textContent;
+    } 
+    else {
+    screen.textContent += this.textContent;
+    }
+}
