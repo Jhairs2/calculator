1d8f88b56543bbba1f6af8741e9633c4eb137bd8
Justin Hairston <jhairst2@vols.utk.edu>
2022-09-05 23:15:52 -0400

    Add some basic operations and a check to see if operationButton is running

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,20 @@
 // Global Variables
+let previousNumber, previousOperand;
+let isRunning = false;
 const screen = document.querySelector(".screen");
 const numButtons = document.querySelectorAll(".num-pad");
 const allClearButton = document.querySelector(".all-clear");
 const deleteButton = document.querySelector(".delete");
+const equalButton = document.querySelector(".equal");
+const operationButtons = document.querySelectorAll(".ops");
+
 
 // Event Listeners
-numButtons.forEach(button => {
-    button.addEventListener("click", appendNum); 
-})
-
+numButtons.forEach(button => button.addEventListener("click", appendNum));
+operationButtons.forEach(button => button.addEventListener("click", operations));
 allClearButton.addEventListener("click", clear);
 deleteButton.addEventListener("click", removeNum);
+equalButton.addEventListener("click", operate);
 
 
 // save values for calculations
@@ -24,24 +28,66 @@ deleteButton.addEventListener("click", removeNum);
 
 // Figure out a way to get buttons to display value on screen
 function appendNum() {
-    if (screen.textContent == 0) {
+
+    if (screen.textContent == 0 || isRunning) {
+        isRunning = false;
         screen.textContent = this.textContent;
+        
     } 
 
     else if (screen.textContent.length < 13) {
-    screen.textContent += this.textContent;
+        screen.textContent += this.textContent;
     }
+
 }
 
+
+// Clear all numbers on screen
 function clear() {
     screen.textContent = "0";
 }
 
+// Remove last number inputted on screen
 function removeNum() {
+    
     if(screen.textContent.length == 1) {
         screen.textContent = "0";
     }
     else {
     screen.textContent = screen.textContent.slice(0, screen.textContent.length - 1);
+    }
+
+}
+
+
+function operations() {
+    isRunning = true;
+    switch(this.textContent) {
+        
+        case "+":
+            previousNumber = screen.textContent;
+            previousOperand = this.textContent;
+
+            break;
+
+        case "-":
+            previousNumber = screen.textContent;
+            previousOperand = this.textContent;
+            break;
+
+    }
+
+}
+
+
+function operate() {
+    let sum = 0;
+    previousNum = previousNumber;
+    previousOp = previousOperand;
+   console.log(previousOp);
+   console.log(previousNum);
+    if (previousOp == "+") {
+        sum = parseFloat(previousNum) + parseFloat(screen.textContent);
+        screen.textContent = sum.toString(); 
     }
 }
