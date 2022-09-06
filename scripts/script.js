2ece93cba82b18372631cbef9a9bf8ddebda6b54
Justin Hairston <jhairst2@vols.utk.edu>
2022-09-05 20:11:57 -0400

    Start js with initial plan

---
+++ scripts/script.js
@@ -0,0 +1,6 @@
+// Figure out a way to get buttons to display value on screen
+// save values for calculations
+// Do calculations with the values selected
+// get math operations to do perform correctly
+// Clear and delete buttons
+// display errors when appropriate.
